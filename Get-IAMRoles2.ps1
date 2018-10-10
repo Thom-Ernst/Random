b652c252bff290e0a5f2688ae9d9cef1d787b9c3
Thom Ernst <thom@ernst.work>
2018-10-10 17:07:06 +0200

    Update Get-IAMRoles2.ps1

--- Get-IAMRoles2.ps1
+++ Get-IAMRoles2.ps1
@@ -1,18 +1,19 @@
 Function Open-RoleGridLevel10 ($list) {
-    $selectedrole = $list | Out-GridView -PassThru
-    if ($selectedrole) {
-        $ent = $selectedrole.nrfEntitlementRef -split "`r`n" | Out-GridView -PassThru
-    }
-    if ($ent) {
-        Open-RoleGridLevel10
-    }
-}
+    #$date = (Get-ChildItem .\roles.csv).LastWriteTime
+    $date = 'TODO - update this code'
+    $roleselected = $list[0]
 
-function Show-IamRoles {
-    $path = '.\roles.csv'
+        $selectedrole = $list | Out-GridView -PassThru -Title "Level10/30 status - $date"
+        if ($selectedrole) {
+            $roleselected = $selectedrole.nrfEntitlementRef -split "`r`n" | Out-GridView -PassThru -Title $selectedrole.cn
+        }
+    }
+
+
+function Parse-Level10Roles {
+    $path = '.\level10.csv'
     $q = import-csv $path
     foreach ($role in $q) {
-    Write-Host $role.cn
         #name
         #show only nl version
         if ($role.nrfLocalizedNames) {
@@ -68,8 +69,36 @@ function Show-IamRoles {
             $role.nrfRequestDef = $newreqdef
         }
     }
-    $global:IAMRoleList = $q
-    Open-RoleGridLevel10 $IAMRoleList
+    $global:Level10RoleList = $q
+    Open-RoleGridLevel10 $Level10RoleList
+}
+
+function Parse-Level30Roles {
+    $path = '.\level30.csv'
+    $q = import-csv $path
+    foreach ($role in $q) {
+        #entitlements
+        #show type and parameter of entitlement
+        if ($role.nrfEntitlementRef) {
+            $entitlements = $role.nrfEntitlementRef -split '\|'
+            $arr = New-Object System.Collections.ArrayList
+            foreach ($entitlement in $entitlements) {
+                $Matches = ''
+                $entitlement -Match 'cn=(.+),cn=.*,cn=.*'
+                $entitlementtype = $Matches[1]
+                $Matches = ''
+                $entitlement -Match '<param>(.+)</param>'
+                $entitlementparam = $Matches[1]
+                $entitlementfull = "$entitlementtype,$entitlementparam"
+                $arr.Add($entitlementfull)
+            }
+            # Row for each nrfentitlement type
+            $newentitlements = $arr -join "`r`n"
+            $role.nrfEntitlementRef = $newentitlements
+        }
+    }
+    $global:Level30RoleList = $q
+    Open-RoleGridLevel10 $Level30RoleList
 }
 
 function Get-IamRole ($search){
@@ -82,3 +111,18 @@ function Get-IamEntitlement ($search){
 }
 
 ## ↑ samenvoegen
+Function Out-EntitlementList ($rolelist) {
+    $processedroles = @()
+    foreach ($role in ($rolelist | where nrfEntitlementRef)) {
+        foreach ($entitlement in ($role.nrfEntitlementRef -split "`r`n")) {
+            $driver,$parameter = $entitlement -split ','
+            $roleobject = [PSCustomObject]@{
+                CN = $role.cn
+                Driver = $driver
+                Parameter = $parameter
+            }
+            $processedroles += $roleobject
+        }
+    }
+    return $processedroles
+}
